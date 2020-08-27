77290303ac19a5417441c4470ff30279e5f483aa
Bao Nguyen <b@nqbao.com>
2020-08-26 23:06:56 -0700

    gateway controller to generate config file

--- workbench-controller/h1st_saas/config.py
+++ workbench-controller/h1st_saas/config.py
@@ -4,6 +4,11 @@ AWS_REGION = "us-west-1"
 
 DYNDB_TABLE = "H1st_saas_workbench"
 
+TRAEFIK_CONF_DIR = os.environ.get(
+    'TRAEFIK_CONF_DIR', 
+    os.path.dirname(__file__) + '/../traefik-conf'
+)
+
 # no trailibg slash
 BASE_URL = "https://cloud.h1st.ai/project"
 
---
+++ workbench-controller/h1st_saas/gateway_controller.py
@@ -0,0 +1,41 @@
+import os
+from h1st_saas import config
+import yaml
+import tempfile
+
+
+class GatewayController:
+    def __init__(self):
+        os.makedirs(config.TRAEFIK_CONF_DIR, exist_ok=True)
+
+    def setup(self, wid, endpoint):
+        cfg_id = "wb-" + wid
+
+        cfg = {'http': {'services': {}, 'routers': {}}}
+
+        cfg['http']['routers'][cfg_id] = {
+            'rule': f"PathPrefix(`/project/{wid}/`)",
+            'service': cfg_id,
+            'middlewares': ['strip_project'],
+        }
+
+        cfg['http']['services'][cfg_id] = {
+            'loadBalancer': {
+                'servers': [{'url': endpoint}]
+            }
+        }
+
+        with tempfile.NamedTemporaryFile(mode="w", delete=False) as f:
+            yaml.dump(cfg, f)
+            f.flush()
+
+            os.rename(f.name, os.path.join(config.TRAEFIK_CONF_DIR, cfg_id + ".yml"))
+
+    def destroy(self, wid):
+        f = os.path.join(config.TRAEFIK_CONF_DIR, "wb-" + wid + ".yml")
+
+        if os.path.exists(f):
+            os.unlink(f)
+            return True
+
+        return False
