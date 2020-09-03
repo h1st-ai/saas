6b9913c0775575b4c622217b0a81c432aa6919e2
Bao Nguyen <b@nqbao.com>
2020-09-02 17:37:46 -0700

    Add python path

--- workbench-controller/h1st_saas/workbench_controller.py
+++ workbench-controller/h1st_saas/workbench_controller.py
@@ -274,7 +274,8 @@ class WorkbenchController:
         envvar = [
             {'name': 'WORKBENCH_ID', 'value': str(wid)},
             {'name': 'WORKSPACE_PATH', 'value': wb_path},
-            {'name': 'H1ST_MODEL_REPO_PATH', 'value': f"${wb_path}/.models"}
+            {'name': 'H1ST_MODEL_REPO_PATH', 'value': f"${wb_path}/.models"},
+            {'name': 'PYTHONPATH', 'value': wb_path},
         ]
 
         if 'workbench_name' in item:
