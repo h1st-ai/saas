e7a7c4997f6adc1451d7e6a7489e2bf333963957
Bao Nguyen <b@nqbao.com>
2020-09-22 15:23:40 -0700

    use different image for staging

---
+++ infra/staging/workbench.tf
@@ -0,0 +1,13 @@
+resource "aws_ecs_task_definition" "workbench" {
+  family                = "workbench-staging"
+  container_definitions = file("./workbench.json")
+
+  volume {
+    name = "efs"
+
+    efs_volume_configuration {
+      file_system_id = "fs-bfdaf7a6"
+      root_directory = "/"
+    }
+  }
+}
